7427b61ae1b276db35503c86bc258a687afe5f86
Felix Sandström <felix@starweb.se>
2017-09-19 21:43:18 +0200

    use ".dev.js" as a suffix for dev js files and ignore dem in repo

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, 'public/js'),
-        filename: '[name].js',
+        filename: '[name].dev.js',
         publicPath: './public'
     },
 
